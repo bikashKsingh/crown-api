df522ffe4b560b7af50ca4e9920320ac5ba891f4
Bikash Kumar Singh <bikashsinghak47@gmail.com>
2025-05-02 13:23:08 +0530

    modify inquiry validation

--- apiValidationSchemas/inquiryValidationSchema.js
+++ apiValidationSchemas/inquiryValidationSchema.js
@@ -33,10 +33,10 @@ module.exports.create = Joi.object({
 
   address: Joi.string().allow("").trim().label("Address"),
   locality: Joi.string().allow("").trim().label("Locality"),
-  city: Joi.string().required().trim().label("City"),
-  state: Joi.string().required().trim().label("State"),
+  city: Joi.string().trim().label("City"),
+  state: Joi.string().trim().label("State"),
   country: Joi.string().trim().required().label("Country"),
-  pincode: Joi.string().required().trim().label("Pincode"),
+  pincode: Joi.string().trim().label("Pincode"),
 
   inquiryStatus: Joi.string().valid(...INQUIRY_STATUS),
 });
